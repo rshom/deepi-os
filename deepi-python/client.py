8e7681a60107e6cb28819fd8f2f2521f6a44014d
Russell Shomberg <russellshomberg@gmail.com>
2022-01-08 18:39:32 -0500

    fast opencv streaming

--- deepi-python/client.py
+++ deepi-python/client.py
@@ -10,44 +10,49 @@ class FeedSocket(Thread):
     def __init__(self,addr='raspberrypi.local',port=8000):
         Thread.__init__(self)                             
         self.addr = (addr,port)                           
-        self.frame = np.empty((640,480), dtype=np.uint8)  
+        self.frame = None
         self.streaming = False
+
         self.sock = socket.socket()
+        print("Connecting")
         self.sock.connect(self.addr)                                          
-        self.conn = self.sock.makefile('rb')            
-        self.start()
+        self.conn = self.sock.makefile('rb')
+        self.grabframe()        
 
-    def run(self):                                                           
-        try:
-            while True:
-                n = struct.calcsize('<L')
-                sz = struct.unpack('<L',self.conn.read(n))[0]
-                stream = io.BytesIO()           
-                stream.write(self.conn.read(sz))
-                stream.seek(0)
-                self.frame = np.frombuffer(stream.read(),dtype=np.uint8)
+    def grabframe(self):
+        n = struct.calcsize('<L')
+        sz = struct.unpack('<L',self.conn.read(n))[0]
+        stream = io.BytesIO()           
+        stream.write(self.conn.read(sz))
+        stream.seek(0)
+        self.frame = np.frombuffer(stream.read(),dtype=np.uint8)
 
-        finally:             
-            self.conn.close()
-            sock.shutdown(1)
-            sock.close()
-            self.join()
+    def run(self):
+        self.streaming = True
+        while self.streaming:
+            self.grabframe()
+        self.conn.close()
+        self.sock.shutdown(1)
+        self.sock.close()
 
-
-
-
-
-
+    def stop(self):
+        self.streaming = False
+        self.join()
 
 
 class CtlSocket():                             
     pass                                       
 
+
 if __name__=='__main__':                       
     import cv2                                 
-    feed = FeedSocket('raspberrypi.local',8000)
-    while feed.is_alive():
-        print(feed.frame)
-    #     cv2.imshow(cv2.imdecode(feed.frame,1))
-    #     cv2.waitKey(1)
-    # cv2.destroyAllWindows() 
+    feed = FeedSocket('192.168.1.169',8000)
+    print("Starting")
+    feed.start()
+    print("Ready")
+    try:
+        while feed.is_alive():
+            cv2.imshow("frame",cv2.imdecode(feed.frame,1))
+            cv2.waitKey(1)
+    finally:
+        cv2.destroyAllWindows() 
