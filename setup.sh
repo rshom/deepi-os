b42ae52ab265608231fb025dd75fc3a33956b290
Russell Shomberg <russellshomberg@gmail.com>
2022-01-08 17:32:26 -0500

    remove pistreamingapp

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ sudo rm /usr/bin/python
 sudo ln -s /usr/bin/python3 /usr/bin/python
 
 wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
+python get-pip.py
 rm get-pip.py
 
 # Python packages
@@ -43,18 +43,18 @@ sudo apt-get -y install python3-picamera
 # NOTE: pirecorder relies on numpy. The RPiZ does not handle that well
 
 # Install pistreamingapp
-git clone https://github.com/rshom/pistreamingapp.git ~/pistreamingapp
-python -m virtualenv ~/pistreamingapp/venv
-source ~/pistreamingapp/venv/bin/activate
-pip install --ignore-installed ws4py flask picamera gunicorn
-deactivate
-sudo cp pistreamingapp.service /etc/systemd/system/pistreamingapp.service
-sudo systemctl start pistreamingapp
-sudo systemctl enable pistreamingapp
-sudo cp pistreamingapp.nginx /etc/nginx/sites-available/pistreamingapp
-sudo ln -s /etc/nginx/sites-available/pistreamingapp /etc/nginx/sites-enabled/pistreamingapp
-sudo systemctl restart nginx
+# git clone https://github.com/rshom/pistreamingapp.git ~/pistreamingapp
+# python -m virtualenv ~/pistreamingapp/venv
+# source ~/pistreamingapp/venv/bin/activate
+# pip install --ignore-installed ws4py flask picamera gunicorn
+# deactivate
+# sudo cp pistreamingapp.service /etc/systemd/system/pistreamingapp.service
+# sudo systemctl start pistreamingapp
+# sudo systemctl enable pistreamingapp
+# sudo cp pistreamingapp.nginx /etc/nginx/sites-available/pistreamingapp
+# sudo ln -s /etc/nginx/sites-available/pistreamingapp /etc/nginx/sites-enabled/pistreamingapp
+# sudo systemctl restart nginx
 
-sudo cp pistreamingapp.nginx /etc/nginx/sites-available/pistreamingapp
-sudo ln -s /etc/nginx/sites-available/pistreamingapp /etc/nginx/sites-enabled/pistreamingapp
-sudo systemctl daemon-reload
+# sudo cp pistreamingapp.nginx /etc/nginx/sites-available/pistreamingapp
+# sudo ln -s /etc/nginx/sites-available/pistreamingapp /etc/nginx/sites-enabled/pistreamingapp
+# sudo systemctl daemon-reload
